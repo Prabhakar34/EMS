e0bab9b797819dac637b72ebd76c60bcc992b14b
Prabhakar34 <67897716+Prabhakar34@users.noreply.github.com>
2021-04-20 16:27:54 +0530

    First Commit..

---
+++ ems/src/main/java/com/ems/EmsApplication.java
@@ -0,0 +1,17 @@
+package com.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+//@EnableEurekaClient
+public class EmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsApplication.class, args);
+	}
+
+}
